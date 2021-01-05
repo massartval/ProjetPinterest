9adace52ae744c9d46f04e645172785d246e4ecb
Yaco-99 <coopman.yannick@gmail.com>
2021-01-05 16:43:56 +0100

    Nothing

--- src/resources/views/profile/dashboard.blade.php
+++ src/resources/views/profile/dashboard.blade.php
@@ -10,8 +10,13 @@
     <img class="profile_picture" src="{{$info->profile_picture_path}}" alt="picture">
     <h2>{{$info->pseudo}}</h2>
 </div>
-<div>
-
+<div class="gallery">
+@foreach($images as $image)
+    <div class="item">
+        <h4>{{$image->title}}</h4>
+        <a href="/image/{{$image->id}}" target="_blank"><img class="images" src="{{asset("storage/$image->path")}}" alt="wtf"></a> 
+    </div>
+@endforeach
 </div>
 
 @endforeach
